cc321c13e0b5c4bd3c82c48427e70b2d9f7ef074
dinhnguyen92 <nguyendinhnguyen92@gmail.com>
2017-02-26 18:28:04 -0800

    Initial setup using .NET MVC

---
+++ WebMVCBasics/WebMVCBasics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMVCBasics.Startup))]
+namespace WebMVCBasics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
